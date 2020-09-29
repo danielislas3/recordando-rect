b261729626798de6527c695100cb788ca75f351d
danielislas3 <danielislas3@live.com>
2020-09-29 16:09:00 -0500

    test con jest

---
+++ src/tests/demo.test.js
@@ -0,0 +1,10 @@
+test("Los strings deben de ser iguales ", () => {
+  //1. Inicialización
+  const mensaje = "Hola mundo";
+
+  //2. Estimiulo
+  const mensaje2 = "Hola mundo";
+
+  //3.Observar comportamiento
+  expect(mensaje).toBe(mensaje2);
+});
