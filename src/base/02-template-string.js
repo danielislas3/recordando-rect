7b4a3e06b5499770c7992c32130fb333a407eb14
danielislas3 <danielislas3@live.com>
2020-10-03 11:42:40 -0500

    test arreglos

--- src/base/02-template-string.js
+++ src/base/02-template-string.js
@@ -8,11 +8,11 @@ const apellido = 'Orio';
 // const nombreCompleto = nombre + ' ' + apellido;
 const nombreCompleto = `${ nombre } ${ apellido }`;
 
-console.log( nombreCompleto );
+// console.log( nombreCompleto );
 
 
 export function getSaludo(nombre='Rodrigo') {
     return 'Hola ' + nombre;
 }
 
-console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
+// console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
---
+++ src/tests/base/08-imp-exp.test.js
@@ -0,0 +1,38 @@
+import "@testing-library/jest-dom";
+
+const { getHeroeById, getHeroesByOwner } = require("../../base/08-imp-exp");
+const { default: heroes } = require("../../data/heroes");
+
+describe("Pruebas en funciones de Héroes", () => {
+  test("debe de retornar un héroe por id", () => {
+    const id = 1;
+    const heroe = getHeroeById(id);
+
+    const heroeData = heroes.find((h) => h.id == id);
+    expect(heroe).toEqual(heroeData);
+  });
+
+  test("debe retornar undefined si heroe no existe", () => {
+    const id = 10;
+    const heroe = getHeroeById(id);
+
+    expect(heroe).toBe(undefined);
+  });
+
+  test("debe retornar arreglo con heroes de DC", () => {
+    const owner = "DC";
+    const heroes = getHeroesByOwner(owner);
+
+    const heroesData = heroes.filter((h) => h.owner === owner);
+
+    expect(heroes).toEqual(heroesData);
+  });
+
+  test("debe retornar arreglo con heroes de MARVEL", () => {
+    const owner = "Marvel";
+
+    const heroes = getHeroesByOwner(owner);
+
+    expect(heroes.length).toBe(2);
+  });
+});
